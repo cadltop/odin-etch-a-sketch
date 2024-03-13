d170d0df5f51db8e71c9538bddd20bf2bd5d4163
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-13 12:41:27 -0500

    Change the iterations that generate grid.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function createGrid() {
+    const gridDiv = document.querySelector('.grid');
+
     for (let i1 = 1; i1 <= 16; i1++) {
+        const newRowDiv = document.createElement('div');
+        newRowDiv.classList.add('column');
+        newRowDiv.classList.add(`c${i1}`);
+        gridDiv.appendChild(newRowDiv);
+        const rowDiv = document.querySelector(`.column.c${i1}`);
+
         for (let i2 = 1; i2 <= 16; i2++) {
-            const gridDiv = document.querySelector('.grid');
-            const squareDiv = document.createElement('div');
-            squareDiv.classList.add('square');
-            gridDiv.appendChild(squareDiv);
+            const newColumnDiv = document.createElement('div');
+            newColumnDiv.classList.add('row');
+            newColumnDiv.classList.add(`r${i2}`);
+            rowDiv.appendChild(newColumnDiv);
         }
     }
 }
