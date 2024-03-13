1d867ad89cdb49348b06e5d7878399a7809db7be
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-13 12:50:37 -0500

    Fix back the iteration.

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function createGrid() {
 
     for (let i1 = 1; i1 <= 16; i1++) {
         const newRowDiv = document.createElement('div');
-        newRowDiv.classList.add('column');
-        newRowDiv.classList.add(`c${i1}`);
+        newRowDiv.classList.add('row');
+        newRowDiv.classList.add(`r${i1}`);
         gridDiv.appendChild(newRowDiv);
-        const rowDiv = document.querySelector(`.column.c${i1}`);
+        const rowDiv = document.querySelector(`.row.r${i1}`);
 
         for (let i2 = 1; i2 <= 16; i2++) {
             const newColumnDiv = document.createElement('div');
-            newColumnDiv.classList.add('row');
-            newColumnDiv.classList.add(`r${i2}`);
+            newColumnDiv.classList.add('column');
+            newColumnDiv.classList.add(`c${i2}`);
             rowDiv.appendChild(newColumnDiv);
         }
     }
