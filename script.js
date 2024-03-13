7b69fb6b7d7719ad580abd16222430620584feb2
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-12 20:55:44 -0500

    Fix the function.
    Put the variable declarations inside the 2 for loops.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function createGrid() {
-    const gridDiv = document.querySelector('.grid');
-    const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-
     for (let i1 = 1; i1 <= 16; i1++) {
         for (let i2 = 1; i2 <= 16; i2++) {
+            const gridDiv = document.querySelector('.grid');
+            const squareDiv = document.createElement('div');
+            squareDiv.classList.add('square');
             gridDiv.appendChild(squareDiv);
         }
     }
